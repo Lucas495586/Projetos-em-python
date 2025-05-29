31dec52f44617fc599caa726a51ee2a842a8e271
ANTONIO LUCAS MOREIRA ROSADO <124186511+Lucas495586@users.noreply.github.com>
2025-05-29 18:43:44 -0300

    Delete Projeto python directory

--- Projeto python/Lista_ordenada.py
+++
@@ -1,8 +0,0 @@
-celsius = float(input('Quantos graus celsius'))
-
-fahrenheit = (celsius * 9 / 5) + 32
-
-kelvin = (celsius + 273.15)
-
-print(celsius, fahrenheit, kelvin)
-     
--- Projeto python/VerificadorPalindromo.py
+++
@@ -1,8 +0,0 @@
-palavra = (input('digite uma palavra'))
-palavra_invertida = palavra[::-1]
-
-if(palavra == palavra_invertida):
-   print('e um palindromo')
-
-else:
-    print("não e palindromo")
